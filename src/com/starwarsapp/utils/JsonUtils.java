43884623bfe910e06198188af88de260d9dc1a1f
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:46:37 -0600

    refactored to static

--- src/com/starwarsapp/utils/JsonUtils.java
+++ src/com/starwarsapp/utils/JsonUtils.java
@@ -5,12 +5,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public abstract class JsonUtils {
-    private Gson gson = new GsonBuilder()
+    private static Gson gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
             .setPrettyPrinting()
             .create();
 
-    public Gson getGson() {
+    public static Gson getGson() {
         return gson;
     }
 }

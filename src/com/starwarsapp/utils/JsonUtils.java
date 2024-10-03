26f4385c0703af4ec33f852563bc932bba1fd859
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:44:22 -0600

    removed methods and added a getter

--- src/com/starwarsapp/utils/JsonUtils.java
+++ src/com/starwarsapp/utils/JsonUtils.java
@@ -3,16 +3,14 @@ package com.starwarsapp.utils;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.starwarsapp.models.Movie;
 
 public abstract class JsonUtils {
-    private static Gson gson = new GsonBuilder()
+    private Gson gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
             .setPrettyPrinting()
             .create();
 
-
-    public static String toJson(Movie data) {
-        return gson.toJson(data);
+    public Gson getGson() {
+        return gson;
     }
 }

1c1174a3af19b7e286b8ba04ef86e552cef56259
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:12:45 -0600

    implemented Movie class with attributes and methods

--- src/com/starwarsapp/models/Movie.java
+++ src/com/starwarsapp/models/Movie.java
@@ -1,4 +1,51 @@
 package com.starwarsapp.models;
 
 public class Movie {
+    private String title;
+    private String plot;
+    private String director;
+    private String producer;
+    private String releaseDate;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String plot, String director, String producer, String releaseDate) {
+        this.title = title;
+        this.plot = plot;
+        this.director = director;
+        this.producer = producer;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "title='" + title + '\'' +
+                ", plot='" + plot + '\'' +
+                ", director='" + director + '\'' +
+                ", producer='" + producer.substring(0, 10) + "..." + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
 }

b5d1827e919ac87637326bd0b1cf111f507c97a0
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:32:10 -0600

    added saveMovieToFile to create .json files

--- src/com/starwarsapp/services/JsonFileWriter.java
+++ src/com/starwarsapp/services/JsonFileWriter.java
@@ -1,4 +1,16 @@
 package com.starwarsapp.services;
 
+import com.starwarsapp.models.Movie;
+import com.starwarsapp.utils.JsonUtils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JsonFileWriter {
+
+    public static void saveMovieToFile(String filePath, Movie movie) throws IOException {
+        FileWriter writer = new FileWriter(filePath + ".json");
+        writer.write(JsonUtils.toJson(movie));
+        writer.close();
+    }
 }

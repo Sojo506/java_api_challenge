3a59434af6da0c105e47373ed658f3b3d3022b1a
Sojo506 <fsojodev@gmail.com>
2024-10-03 11:17:09 -0600

    modified gson variable to a final variable

--- src/com/starwarsapp/utils/JsonUtils.java
+++ src/com/starwarsapp/utils/JsonUtils.java
@@ -5,7 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public abstract class JsonUtils {
-    private static Gson gson = new GsonBuilder()
+    private static final Gson gson = new GsonBuilder()
             .setPrettyPrinting()
             .create();
 

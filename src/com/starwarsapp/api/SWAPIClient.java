71ab039eb2f9d1f3077f7c881f7ec77350359f5e
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:20:02 -0600

    converted class to abstract, and implemented getData method

--- src/com/starwarsapp/api/SWAPIClient.java
+++ src/com/starwarsapp/api/SWAPIClient.java
@@ -1,4 +1,24 @@
 package com.starwarsapp.api;
 
-public class SWAPIClient {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public abstract class SWAPIClient {
+    private static String url = "https://swapi.py4e.com/api/films/";
+
+    public static String getData(String index) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + index))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+
+        return json;
+    }
 }

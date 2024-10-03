edb10b2ad56127c4b6ee654c365a831cedbb770e
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:30:48 -0600

    converted class to abstract, and implemented toJson method

--- src/com/starwarsapp/utils/JsonUtils.java
+++ src/com/starwarsapp/utils/JsonUtils.java
@@ -1,4 +1,18 @@
 package com.starwarsapp.utils;
 
-public class JsonUtils {
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.starwarsapp.models.Movie;
+
+public abstract class JsonUtils {
+    private static Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .setPrettyPrinting()
+            .create();
+
+
+    public static String toJson(Movie data) {
+        return gson.toJson(data);
+    }
 }

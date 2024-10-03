2e1db9a8cdcf84b971af3ab919301d5e3c2644f0
Sojo506 <fsojodev@gmail.com>
2024-10-03 10:41:47 -0600

    modified format of url to json

--- src/com/starwarsapp/api/SWAPIClient.java
+++ src/com/starwarsapp/api/SWAPIClient.java
@@ -7,18 +7,20 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public abstract class SWAPIClient {
-    private static String url = "https://swapi.py4e.com/api/films/";
+    private static String url = "https://swapi.py4e.com/api/films/%s/?format=json";
 
     public static String getData(String index) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url + index))
+                .uri(URI.create(String.format(url, index)))
                 .build();
+        System.out.println("request = " + request);
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("response = " + response);
 
         String json = response.body();
-
+        System.out.println("JSON: " + json);
         return json;
     }
 }

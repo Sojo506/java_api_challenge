54886ea14b93b1728b4713bb5145cfe89bca2b66
Sojo506 <fsojodev@gmail.com>
2024-10-03 11:18:03 -0600

    tested v01

--- src/com/starwarsapp/Main.java
+++ src/com/starwarsapp/Main.java
@@ -1,7 +1,22 @@
 package com.starwarsapp;
 
-public class Main {
-    public static void main(String[] args) {
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.starwarsapp.api.SWAPIClient;
+import com.starwarsapp.models.Movie;
+import com.starwarsapp.utils.JsonUtils;
+import com.starwarsapp.utils.MovieSWAPI;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Gson gson = JsonUtils.getGson();
+
+        String json = SWAPIClient.getData("2");
+        MovieSWAPI movieSWAPI = gson.fromJson(json, MovieSWAPI.class);
+        Movie movie = new Movie(movieSWAPI);
+        System.out.println("movie = " + movie);
     }
 }
